2408d4cdb081b2206a90652b44e310ded6c67844
Apriadi <dev.apriadi@gmail.com>
2021-07-05 10:25:00 +0700

    membuat tombol login menjadi ketengah

--- src/screen/login/LoginScreen.js
+++ src/screen/login/LoginScreen.js
@@ -6,8 +6,8 @@ const LoginScreen = ({ navigation }) => {
     return(
         <View style={{flex : 1}}>
            <ImageBackground style={{flex : 1}} source={backgroundGambar}>
-            <View style={{flex: 1, justifyContent : 'flex-end', marginBottom : 110, marginHorizontal : 20}}>
-                  <View style={{ width: 230 }}>
+            <View style={{flex: 1, justifyContent : 'flex-end', alignItems: 'center', marginBottom : 120, marginHorizontal : 20}}>
+                  <View style={{ width: 250 }}>
                     <Button title={'Login'} color={'#AAE296'} onPress={() => navigation.navigate('PhoneLoginScreen')}/>
                   </View>
               </View>
